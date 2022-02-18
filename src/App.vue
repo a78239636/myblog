bc37662e256e20cb3b4156fa1f8436351720cd44
LZR <a78239636@163.com>
2022-02-18 17:06:57 +0800

    add item example

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,48 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="add"> 按我添加物品 </button>
+    <button v-on:click="minus"> 按我减少物品 </button>
+    <div>
+      <ul id="myList">
+        <li v-for="item in items" v-bind:key="item.message">
+          {{item.message}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import 'element-ui/lib/theme-chalk/index.css'
+
+
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      count : 1,
+      items : [  ]
+    }
+  },
+
+  methods : {
+      add() {
+        this.count++;
+        let obn = {message : "物品" + this.count};
+        console.log(obn)
+        this.items.push(obn)
+      },
+      minus() {
+        this.count--;
+      //  let obn = {message : "物品" + this.count};
+        this.items.pop();
+      }
   }
 }
 </script>
