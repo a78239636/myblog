b62ee6d6c2b6313ff26202dd724a6684eb66cc63
LZR <a78239636@163.com>
2022-03-15 09:16:34 +0800

    调整目录结构，删除冗余

---
+++ src/components/toDoItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li> "代办事项：" +  {{ todo.text }}  + "紧急程度" + {{ todo.level }} </li>
+</template>
+
+<script>
+export default {
+  name: "toDoItem",
+  props: ['todo']
+}
+</script>
+
+<style scoped>
+</style>
